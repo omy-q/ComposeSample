08a2451c64b42e826b33ece3f134d47d952b6e88
omy-q <olya_aparina@mail.ru>
2023-03-24 22:07:03 +0300

    lesson3

--- app/src/main/java/com/example/composesample/HomeScreen.kt
+++ app/src/main/java/com/example/composesample/HomeScreen.kt
@@ -1,8 +1,13 @@
 package com.example.composesample
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 
@@ -11,7 +16,13 @@ fun HomeScreen() {
     Text(
         text = "Home Screen",
         fontSize = 32.sp,
-        color = Color.Magenta
+        color = Color.White,
+        overflow = TextOverflow.Ellipsis,
+        maxLines = 1,
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .background(color = Color.Black)
+            .fillMaxWidth()
     )
 }
 
